56a083fee338a47c1d134d1a15eda74f9c5223e4
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-09-02 19:29:35 -0700

    Add r_info(), which is powerful, and r_libs_user()

--- R/rwasm.R
+++ R/rwasm.R
@@ -18,7 +18,6 @@ install_webr <- function(path = node_path()) {
 #' is because it queries `getRversion()` in webR.
 #'
 #' @examplesIf interactive()
-#' webr_version()
 #' r_version()
 #' r_version("x.y")
 #'
@@ -27,7 +26,7 @@ webr_version <- local({
   version <- NULL
   function() {
     if (is.null(version)) {
-      esm_code <- 'import { WebR } from "webr"; const webr = new WebR(); console.log(webr.version); process.exit(0);'
+      esm_code <- 'import { WebR } from "webr"; const webr = new WebR(); console[lo.log(webr.version); process.exit(0);'
       out <- node(args = c("--input-type=module", "-e", shQuote(esm_code)))
       version <<- numeric_version(out)
     }
@@ -36,40 +35,70 @@ webr_version <- local({
 })
 
 
-#' Get the R version that webR provides
+#' Get R version information on the R instance that webR provides
 #'
-#' @param format (optional; character string) If `"x.y"`, then the
-#' 'x.y' component of the R version 'x.y.z' is returned.
+#' @param template (optional; character string) A template with conversion
+#' specifiers as described in [base::R_LIBS_USER] that are expanded.
 #'
 #' @return
-#' `r_version()` returns the version of R that webR implements.
+#' `r_version()` returns `R.version()` in webR and `r_version(template)`
+#' returns a character string based on the format in webR.
+#'
+#' @examplesIf interactive()
+#' r_info()
+#' r_info("~/R/%p-library/%v")
 #'
 #' @rdname webr_version
 #' @export
-r_version <- local({
-  version <- NULL
-  function(format = NULL) {
-    if (is.null(version)) {
-      esm_code <- 'import { WebR } from "webr"; const webr = new WebR(); await webr.init(); await webr.evalR("cat(as.character(getRversion()))"); process.exit(0);'
+r_info <- local({
+  info <- NULL
+  function(template = NULL) {
+    if (is.null(info)) {
+      esm_code <- 'import { WebR } from "webr"; const webr = new WebR(); await webr.init(); await webr.evalR("cat(deparse1(R.version))"); process.exit(0);'
       out <- node(args = c("--input-type=module", "-e", shQuote(esm_code)))
-      version <<- R_system_version(out)
+      info <<- eval(parse(text = out))
     }
 
-    if (!is.null(format)) {
-      if (format == "x.y") {
-        version <- paste(unlist(version)[1:2], collapse = ".")
-        version <- package_version(version)
-      }
+    if (is.null(template)) return(info)
+    
+    stopifnot(is.character(template), length(template) == 1L)
+    res <- template
+    for (spec in c("%V", "%v", "%p", "%o", "%a")) {
+      if (!grepl(spec, res)) next
+      value <- switch(spec,
+        "%V" = paste(info[["major"]], info[["minor"]], sep = "."),
+        "%v" = paste(info[["major"]], sub("[.].*", "", info[["minor"]]), sep = "."),
+        "%p" = info[["platform"]],
+        "%o" = info[["os"]],
+        "%a" = info[["arch"]],
+               stop("Unknown specifier: ", spec)
+      )
+      res <- gsub(spec, value, res, fixed = TRUE)
     }
-    version
+    res
   }
 })
 
 
-webr_version <- function() {
-  ver <- node(args = c("--input-type=module", "-e", shQuote('import { WebR } from "webr"; const webr = new WebR(); console.log(webr.version); process.exit(0);')))
-  numeric_version(ver)
+
+#' Manage the R package library for webR
+#'
+#' @param path (character string) The path to the R package library
+#' on the host file system to be mounted as the R user package library
+#' in webR. The path supports some of the 
+#'
+#' @return
+#' `r_libs_user()` returns the package library path for webR in
+#' `~/R/%p-library/%v`, where `%p` is the R platform and `%v` is the
+#' R 'x.y' version R  in webR as extracted by [r_info()], e.g.
+#' `~/R/wasm32-unknown-emscripten-library/4.5`.
+#' 
+#' @export
+r_libs_user <- function(path = "~/R/%p-library/%v") {
+  if (grepl("%.", path)) path <- r_info(path)
+  path
 }
+
 
 
 get_rwasm_rscript <- function(path = node_path(), must_work = TRUE) {
